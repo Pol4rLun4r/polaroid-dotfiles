f149422021032bf26fa4b0bae8cdee146eb8b03c
Pol4rLun4r <qu3ij0@hotmail.com>
2025-05-01 08:21:26 -0300

    ✨ feat(install-all): adicionar confirmação de prosseguir com a instalação das dotfiles
    adicionado confirmação, perguntando para o usuário se ele deseja mesmo prosseguir com a instalação das dotfiles, alertando que será necessário reiniciar a sessão algumas vezes

--- install-all.sh
+++ install-all.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
-# habilitá o sudo por padrão
-sudo echo ""
+# habilitá o sudo no inicio
+sudo -v
 
 # pasta padrão dos dotfiles
-SRC_BASE="$HOME/.dotfiles"
+SRC_BASE=$(dirname $(realpath "$0"))
+
+read -p "📑 Começando a instalação das dotfiles, ao iniciar será pedido para reiniciar a sessão algumas vezes, deseja prosseguir? (y/n):" CONFIRM_INIT
+echo
+
+if [[ "$CONFIRM_INIT" =~ ^[yY]$ ]]; then
 
 # source "$SRC_BASE/wallpapers/apply-wallpapers.sh
 
 # instala os apps no sistema
-bash "$SRC_BASE/apps/main.sh" 
+# bash "$SRC_BASE/apps/main.sh" 
 
 # aplica o tema de ícones no sistema
-bash "$SRC_BASE/icons/main.sh"
+# bash "$SRC_BASE/icons/main.sh"
 
 # baixa e instala as extensões no sistema
 # bash "$SRC_BASE/gnome/install-gnome-extensions.sh"
 
 # corrige o erro da tela de bloqueio do gnome juntamente com as extensões
-bash "$SRC_BASE/gnome/fix-screen-lock/main.sh"
+# bash "$SRC_BASE/gnome/fix-screen-lock/main.sh"
 
-echo "🔁 Deseja fazer logout para aplicar todas as mudanças? (y/n):"
-read CONFIRM 
+else
+    echo "🔸 Instalação dos dotfiles cancelada"
+    exit 0
+fi
+
+read -p "🔁 Deseja fazer logout para aplicar todas as mudanças? (y/n):" CONFIRM
 
 if [[ "$CONFIRM" =~ ^[yY]$ ]]; then
     echo "🔧 saindo em..."
