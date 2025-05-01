d6411a6f08f83263a5c5f8ca8993eecb13199375
Pol4rLun4r <qu3ij0@hotmail.com>
2025-05-01 15:00:28 -0300

    ♻️ refactor(extensions): adicionar verificações de validação ao baixar extensões
    adicionado verificações no arquivo install-gnome-extensios.sh para verificar se já existe o download da extensão, se teve sucesso ao baixar e ao extrair

--- gnome/install-gnome-extensions.sh
+++ gnome/install-gnome-extensions.sh
@@ -1,84 +1,91 @@
 #!/bin/bash
 
-# script para baixar e instalar extensões gnomes
+# script para baixar e instalar extensões do GNOME
 
-sudo echo ""
+# solicita senha do sudo para manter a sessão ativa
+sudo -v
 
-# lista com as extensões gnome
-LIST="$HOME/.dotfiles/gnome/extensions-enabled.txt"
+# Diretório atual
+CURRENT_DIR=$(dirname $(realpath "$0"))
 
-# versão do gnome-shell
+# caminho do arquivo com UUIDs das extensões
+LIST="$CURRENT_DIR/extensions-enabled.txt"
+
+# Obtém versão major do GNOME Shell (ex: 44, 45)
 SHELL_VERSION=$(gnome-shell --version | cut -d ' ' -f 3 | cut -d '.' -f 1)
 
-# Verificar se o arquivo de extensões existe
+# Verifica se o arquivo existe
 if [ ! -f "$LIST" ]; then
     echo "❌ Arquivo extensions-enabled.txt não encontrado!"
     exit 1
 fi
 
-# Instalar extensões listadas
-echo "📋 Instalando extensões do arquivo extensions-enabled.txt..."
+# cria pasta temporária para downloads
+mkdir -p /tmp/ext
 
-echo "🔍 Procurando extensões..."
+echo -e "📋 Instalando extensões do arquivo extensions-enabled.txt...\n"
+echo -e "🔍 Procurando extensões...\n"
 
 while read -r uuid; do
-    # Buscar usando UUID + Shell Version
+    # busca info da extensão
     info=$(curl -s "https://extensions.gnome.org/extension-info/?uuid=$uuid&shell_version=$SHELL_VERSION" | jq -r '.download_url')
-    
+
     if [ "$info" == "null" ] || [ -z "$info" ]; then
         echo "❌ Extensão '$uuid' não encontrada ou incompatível com GNOME $SHELL_VERSION."
         continue
     fi
 
-
-    # Baixar e instalar extensão pela url
+    # Baixa a extensão
     name_file=$(echo "$uuid" | cut -d '@' -f 1)
-    TEMP_FILE="/tmp/ext/$name_file"
-
+    TEMP_FILE="/tmp/ext/$name_file.zip"
     download_url="https://extensions.gnome.org/$info"
 
-    curl -sL \
-    -H "User-Agent: Mozilla/5.0" \
-    -H "Referer: https://extensions.gnome.org/" \
-    "$download_url" \
-    -o "$TEMP_FILE"
-
+    # verifica se a extensão já existe
     if [ -f "$TEMP_FILE" ]; then
-        echo "❗ Falha na tentativa de baixar: $name_file"
+        echo "💾 A seguinte extensão já foi baixada: $name_file"
         continue
-    fi
-
-    EXT_DIR="$HOME/.local/share/gnome-sell/extensions/$uuid"
-    mkdir -p "$EXT_DIR"
-    unzip -qo "$TEMP_FILE" -d "$EXT_DIR"
-    rm -r "$TEMP_FILE"
-
-    if [ -f "$EXT_DIR" ]; then
-        echo "❗ Falha ao extrair: $name_file"
-        continue
-    fi    
-
-   if gnome-extensions enable "$uuid" 2> /dev/null; then
-        echo "✅ Extensão ativada com sucesso!"
     else
-        echo "❌ Falha ao ativar a extensão ou ela não está instalada."
-    fi
+        curl -sL \
+        -H "User-Agent: Mozilla/5.0" \
+        -H "Referer: https://extensions.gnome.org/" \
+        "$download_url" \
+        -o "$TEMP_FILE"
 
+        # verifica se teve sucesso ao baixar
+        if [ -f "$TEMP_FILE" ]; then
+            echo "✅ Sucesso ao baixar: $name_file"
+        else
+            echo "❗ Falha ao baixar: $name_file"
+            continue
+        fi
+
+        # extrai a arquivo da extensão
+        EXT_DIR="$HOME/.local/share/gnome-shell/extensions/$uuid"
+        mkdir -p "$EXT_DIR"
+
+        if ! unzip -qo "$TEMP_FILE" -d "$EXT_DIR"; then
+            echo "❌ Falha ao extrair o arquivo: $TEMP_FILE"
+            continue
+        fi
+
+        rm -f "$TEMP_FILE"
+    fi
 done < "$LIST"
 
-echo "✅ Todas as extensões instaladas e ativadas!"
+# {
 
-echo "💽 Talvez seja necessário reiniciar a sessão para que todas as extensões funcionem com exito"
-
-
-# Ativar extensões instaladas
-# echo "🔧 Ativando extensões instaladas..."
-
-# sleep 3
-
-# while read -r uuid; do
-#     if [ ! -z "$uuid" ]; then
-#         echo "🔹 Ativando extensão: $uuid"
-#         gnome-extensions enable "$uuid"
+#     if [ ! -d "$EXT_DIR" ]; then
+#         echo "❗ Falha ao extrair: $name_file"
+#         continue
 #     fi
-# done < "$LIST"
+
+#     if gnome-extensions enable "$uuid" 2> /dev/null; then
+#         echo "✅ $uuid ativada com sucesso!"
+#     else
+#         echo "❌ Falha ao ativar a extensão '$uuid'."
+#     fi
+# }
+
+
+echo -e "\n✅ Todas as extensões processadas!"
+echo "💽 Talvez seja necessário reiniciar a sessão para que todas as extensões funcionem corretamente."
