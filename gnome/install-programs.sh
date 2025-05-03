3601e4d0070ce1e4b1ae1fe65535d152a8df6a90
Pol4rLun4r <qu3ij0@hotmail.com>
2025-05-03 19:05:13 -0300

    ✨ feat(extensions): adicionar script para instalar gnome-tweaks e manager-extensions

---
+++ gnome/install-programs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# script para instalar gnome-tweaks e manager-extensions
+
+sudo -v
+
+# instala gnome-tweaks
+sudo apt install gnome-tweaks -y
+
+# instala manager-extensions
+sudo apt install gnome-shell-extension-manager
