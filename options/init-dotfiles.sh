99de919e2828e6938f694b7a9a276c3657999f3a
Pol4rLun4r <qu3ij0@hotmail.com>
2025-08-06 11:16:19 -0300

    🔧 chore(init): desativar menu de configurações secundárias temporariamente
    - Comentado item "(2) Configurações e instalações secundárias" no menu inicial
    - A entrada agora aponta para "Reverter Instalações Principais"
    - Leitura do input ajustada de [1/2/3/q] para [1/2/q]
    - Comentários adicionados explicando a desativação temporária

--- options/init-dotfiles.sh
+++ options/init-dotfiles.sh
@@ -14,17 +14,17 @@ function initDotFiles() {
 
     tput bold;  
     echo -e "(1) Instalações Principais: Extensões, Wallpapers, Apps e Icons.\n"
-    echo -e "(2) Configurações e instalações secundárias.\n"
-    echo -e "(3) Reverter Instalações Principais\n";
+    # echo -e "(2) Configurações e instalações secundárias.\n" # desativado temporariamente até funcionalidades estarem prontas 
+    echo -e "(2) Reverter Instalações Principais\n";
     tput sgr0
     echo -e "(q) Sair e não fazer nada.\n"
 
-    tput bold; read -p "Escolha [1/2/3/q]: " OPTIONS; tput sgr0
+    tput bold; read -p "Escolha [1/2/q]: " OPTIONS; tput sgr0
 
     case $OPTIONS in 
         1) mainFacilities ;;
-        2) clear; echo "Instalando... others" ;;
-        3) revertScript ;; 
+        # 2) clear; echo "Instalando... others" ;;
+        2) revertScript ;; 
         q) clear; exit 0 ;;
         *) initDotFiles ;;
     esac
