339bb0e4e8328f118015567a89acbf3bb64c8c81
Pol4rLun4r <qu3ij0@hotmail.com>
2025-08-13 10:37:47 -0300

    ✨ feat(secondary-features): adicionar opção para instalar GnomeTweaks
    - Substituída mensagem placeholder por menu interativo de configurações extras
    - Adicionada opção `(1)` para instalação do **GnomeTweaks** via script dedicado
    - Incluídas opções `(b)` para voltar ao menu principal e `(q)` para sair sem alterações
    - Garantida reexibição do submenu em caso de entrada inválida

---
+++ gnome/install-gnome-tweaks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# script para instalar o gnome-tweaks
+sudo -v
+
+# instala gnome-tweaks
+sudo apt install gnome-tweaks -y
--- options/secondary-features.sh
+++ options/secondary-features.sh
@@ -3,5 +3,20 @@
 function secondaryFeatures() {
     clear
     
-    echo "Construction Screen..."
+    echo -e "⚙️  Quais configurações extras deseja fazer?\n"
+
+    tput bold;
+    echo -e "(1) Instalar GnomeTweaks\n"
+    tput sgr0
+    echo -e "(b) Voltar para o Menu\n"
+    echo -e "(q) Sair e não fazer nada.\n"
+
+    tput bold; read -p "Escolha [1/2/3/4/b/q]: " OPTIONS; tput sgr0    
+
+    case $OPTIONS in 
+        1) clear; bash "$SRC_BASE/gnome/install-gnome-tweaks.sh" ;;
+        b) bash "$SRC_BASE/main.sh" ;;
+        q) clear; exit 0 ;;
+        *) secondaryFeatures ;;
+    esac
 }
